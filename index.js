5f261dd23fae76bc3d8fdf6801527e9b96e5a40d
Nima Jamalian <nima.jamalian80@gmail.com>
2021-10-02 14:12:46 +0100

    Animation + Winner Colour

--- index.js
+++ index.js
@@ -22,18 +22,35 @@ function diceManager() {
   diceRolls.push(randomDiceRollGenerator());
   diceRolls.push(randomDiceRollGenerator());
 
+  //Changing Dice Image
   //diceRolls[0] -> Player 1
   //diceRolls[1] -> Player 2
   document.querySelector(".playerOneDice").className =
     "playerOneDice fas img" + " fa-dice-" + diceRollsNumber[diceRolls[0] - 1];
   document.querySelector(".playerTwoDice").className =
     "playerTwoDice fas img" + " fa-dice-" + diceRollsNumber[diceRolls[1] - 1];
+
+  //Dice 1 & 2 Animation
+  document.querySelector(".playerTwoDice").classList.add("clickAnimation");
+  var dice2 = document.querySelector(".playerTwoDice");
+  dice2.addEventListener("animationend", () => {
+    document.querySelector(".playerTwoDice").classList.remove("clickAnimation");
+  });
+  document.querySelector(".playerOneDice").classList.add("clickAnimation");
+  var dice1 = document.querySelector(".playerOneDice");
+  dice1.addEventListener("animationend", () => {
+    document.querySelector(".playerOneDice").classList.remove("clickAnimation");
+  });
+  
+  //Deciding the winner
   if (diceRolls[0] > diceRolls[1]) {
     document.querySelector(".result").textContent = "🎉 Player 1 Wins!";
     document.querySelector(".dice1").classList.remove("diceCelebrate");
+    document.querySelector(".playerOneDice").classList.add("winnerDice");
   } else if (diceRolls[0] < diceRolls[1]) {
     document.querySelector(".result").textContent = "Player 2 Wins! 🎉";
     document.querySelector(".dice2").classList.remove("diceCelebrate");
+    document.querySelector(".playerTwoDice").classList.add("winnerDice");
   } else if (diceRolls[0] == diceRolls[1]) {
     document.querySelector(".result").textContent = "Draw! 🏳️";
     document.querySelector(".dice1").classList.add("diceCelebrate");
